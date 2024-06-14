0e73fd782f54eedaadd3bf0eb91271204a07ea91
Lokesh-5618 <121171678+Lokesh-5618@users.noreply.github.com>
2024-06-14 10:59:34 +0530

    Update repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(word,times) {
+if(times<0)
+{
+  return 0
 };
+let st=" "
+for(let i=o;i<times;i++)
+  {
+    st=st+word;
+  }
+return st
+}
 
 // Do not edit below this line
 module.exports = repeatString;
